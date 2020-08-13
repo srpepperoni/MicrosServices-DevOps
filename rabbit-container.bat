8f4290294e25b341e2e33f611da339e4eb353e2f
srpepperoni <jaimeyera@gmail.com>
2020-08-13 22:23:58 +0200

    Autenticacion para usuarios

--- rabbit-container.bat
+++ rabbit-container.bat
@@ -1,7 +1,3 @@
 @echo off
 
-docker run -p 15672:15672 \
--p 5672:5672 -d \
---hostname myrabbit \
---name myrabbit \
-rabbitmq:3-management
+docker run -p 15672:15672 -p 5672:5672 -d --hostname myrabbit --name myrabbit rabbitmq:3-management
